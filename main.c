2b00681d878691f86dab153b638ff3398ea8b1ba
Sitian Jiang <jst_1994@qq.com>
2017-04-14 22:14:23 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,16 @@
 
 int main()
 {
-int i,j;
-for(i=4;i<=100;i+=2)
-{
-	for(j=2;j<=i/2;j++)
+	int i,j;
+	for(i=5;i<=1000000;i+=2)/*从5开始依次加2遍历1000000内的数*/	
 	{
-		if(isPrime(j)&&isPrime(i-j))
+		for(j=2;j<=i/2;j++)
 		{
-			printf("%d=%d+%d\n",i,j,i-j);
+			if(i%j==0)
+				printf("%d不是质数"，i);
 		}
+		
 	}
-}
-
+	return 0;
 
 }
